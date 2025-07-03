7e6039cb614c2c8d6ec70bf458f4550a4126edf2
efra8ab <efra8ab@gmail.com>
2025-07-03 10:22:43 -0600

    pnpm installed

--- server.js
+++ server.js
@@ -6,9 +6,11 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts =  require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const expressEjsLayouts = require("express-ejs-layouts")
 
 /* ***********************
  * Routes
