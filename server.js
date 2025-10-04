036617adeecf5b0acde26cddd391903136f17435
efra8ab <efra8ab@gmail.com>
2025-10-04 03:07:01 -0600

    W02 activities

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController");
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * View Engine and Templates
@@ -23,9 +25,10 @@ app.set("layout", "./layouts/layout")
  *************************/
 app.use(static) //we tell the app where to locate our static stuff and can access it now
 //Index route
-app.get("/", function (req, res) {
-  res.render("index", {title: "Home"})
-})
+app.get("/", baseController.buildHome)
+// Inventory routes
+app.use("/inv", inventoryRoute)
+
 
 /* ***********************
  * Local Server Information
