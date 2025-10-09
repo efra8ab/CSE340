5634021fd4d160892aef4b89df2b1cfbd57beec3
efra8ab <efra8ab@gmail.com>
2025-10-09 03:03:41 -0600

    learning activities W04

---
+++ routes/accountRoute.js
@@ -0,0 +1,20 @@
+// needed resources
+
+const express = require('express');
+const router = new express.Router();
+const accountController = require('../controllers/accountController')
+const utilities = require('../utilities/');
+const { post } = require('./static');
+const regValidate = require("../utilities/account-validation");
+
+// route to build login section
+router.get('/login', utilities.handleErrors(accountController.buildLogin))
+// route to build registration 
+router.get('/registration', utilities.handleErrors(accountController.buildRegister))
+// route to post registration of user
+router.post('/registration',
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount))
+
+module.exports = router;
